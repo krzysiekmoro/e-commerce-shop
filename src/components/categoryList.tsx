a1cb2a62fe49b255c6092af4eb80459dbab5bc60
krzysiekmoro <moro.krzysiek@gmail.com>
2020-10-30 19:26:03 +0100

    Added Category List Component

---
+++ src/components/categoryList.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from 'react'
+
+interface Props {
+    selected: string,
+    categories: string[],
+    selectCategory: (category: string) => void
+}
+
+export const CategoryList: FunctionComponent<Props> = (props) => {
+    return <div>
+        { ['All',...props.categories].map(c => {
+            let btnClass = props.selected === c ? 'btn-selected' : 'btn-unselected'
+            return <button key={ c } onClick={ () => props.selectCategory(c) }
+                className={ `${ btnClass }` }>
+                { c }
+            </button>
+        })}
+    </div>
+}
