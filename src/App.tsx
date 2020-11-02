53b7f27012dcdffe01f70edbcd4cbd0f5af23653
krzysiekmoro <moro.krzysiek@gmail.com>
2020-11-02 18:21:18 +0100

    Ready Components without Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import './App.css';
+import { Product, Order } from './data/handleOrder';
+import { ProductList } from './components/productList';
+import { products } from './data/data';
 
-const App: React.FC = () => {
-  return (
-    <div className="App bg-orange-400">
-      <p>Hello World!</p>
-    </div> 
-  )
-}
+const App: FunctionComponent = () => {
+    const [order, setOrder] = useState<Order>(new Order());
+    const categories = products.map((p) => p.category);
+
+    const addToOrder = (product: Product, quantity: number) => {
+        setOrder((order) => {
+            order.addProduct(product, quantity);
+            return order;
+        });
+    };
+
+    return (
+        <div className='App bg-orange-400'>
+            <ProductList
+                products={products}
+                categories={categories}
+                order={order}
+                addToOrder={addToOrder}
+            />
+        </div>
+    );
+};
 
 export default App;
