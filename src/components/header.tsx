f80159c004ca764c82561a02e562a8f6ce4d95d5
krzysiekmoro <moro.krzysiek@gmail.com>
2020-11-02 16:05:34 +0100

    Added Header Component

---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import React, {FunctionComponent} from 'react'
+import {Order} from '../data/handleOrder'
+
+interface Props {
+    order: Order
+}
+
+export const Header: FunctionComponent<Props> = (props) => {
+    let count = props.order.productCount
+    return <div>
+        { count === 0 ? '(No Products)' : `${count} product(s), ${props.order.total.toFixed(2)}` }
+        <button>
+            Submit Order
+        </button>
+    </div>
+}
