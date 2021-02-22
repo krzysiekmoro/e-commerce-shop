bd9a36536103e166880d06e61a7b61b6e70851ff
krzysiekmoro <moro.krzysiek@gmail.com>
2021-02-22 20:58:40 +0100

    item details styling

--- src/components/ItemDetails.tsx
+++ src/components/ItemDetails.tsx
@@ -20,28 +20,32 @@ export const ItemDetails: FunctionComponent<Props> = (props) => {
                 <img alt='one of the product' src={product.image} />{' '}
             </div>
 
-            <p className='pick'>choose quantity</p>
-            <select onChange={(e) => setQuantity(Number(e.target.value))}>
-                <option value='1'>1</option>
-                <option value='2'>2</option>
-                <option value='3'>3</option>
-            </select>
             <div className='product'>
                 <p>{product.category}</p>
                 <h1>{product.title}</h1>
                 <h2>${product.price.toFixed(2)}</h2>
                 <p className='desc'>{product.description}</p>
                 <div className='buttons'>
-                    <button
+                    <Button
                         className='add'
                         onClick={() => props.callback(product, quantity)}
                     >
                         Add to Cart
-                    </button>
-                    <button className='like'>
+                    </Button>
+                    <Button className='like'>
                         <span>♥</span>
-                    </button>
+                    </Button>
                 </div>
+                <form>
+                    <label className='pick'>choose quantity</label>
+                    <select
+                        onChange={(e) => setQuantity(Number(e.target.value))}
+                    >
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                    </select>
+                </form>
             </div>
         </Container>
     );
--- src/styles/itemDetails.styles.ts
+++ src/styles/itemDetails.styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 
-const white = '#F5F5F5';
-const light = '#E0C9CB';
+const white = '#fff';
+// const light = '#E0C9CB';
 const tan = '#C3A1A0';
 const pink = '#D9AAB7';
 const rose = '#BA7E7E';
@@ -12,112 +12,89 @@ const dark = '#4E4E4E';
 export const Container = styled.div`
     position: relative;
     margin: auto;
+    margin-top: 20px;
     overflow: hidden;
     width: 1040px;
     height: 700px;
     background: ${white};
     box-shadow: 5px 5px 15px ${rose};
     border-radius: 10px;
+    display: flex;
+    flex-flow: row wrap;
     
-    p {
-        font-size: 0.6em;
-        color: ${rose};
-        letter-spacing: 1px;
+    .images{
+        width: 50%;
+
+        img {
+            width: 290px;
+            margin: 47px auto 0px auto;
+        }
     }
-      
-    h1 {
-        font-size: 1.2em;
-        color: ${dark};
-        margin-top: -5px;
-    }
-      
-    h2 {
-        color: ${tan};
-        margin-top: -5px;
-    }
-      
-    img {
-        width: 290px;
-        margin-top: 47px;
-    }
-      
-      
+  
     .product {
-        position: absolute;
+        margin: 5%;
         width: 40%;
-        height: 100%;
-        top: 10%;
-        left: 60%;
-    }
-      
-    .desc {
-        text-transform: none;
-        letter-spacing: 0;
-        margin-bottom: 17px;
-        color: ${dark};
-        font-size: .7em;
-        line-height: 1.6em;
-        margin-right: 25px;
-        text-align: justify;
-    }
-      
-    button {
-        background: darken(${light}, 10%);
-        padding: 10px;
-        display: inline-block;
-        outline: 0;
-        border: 0;
-        margin: -1px;
-        border-radius: 2px;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        color: ${white};
-        cursor: pointer;
 
-        &:hover {
-          background: ${rose};
-          transition: all .4s ease-in-out;
+        p {
+            font-size: 0.9em;
+            color: ${rose};
+            letter-spacing: 1px;
+        }
+          
+        h1 {
+            font-size: 1.5em;
+            color: ${dark};
+            margin-top: -5px;
+        }
+          
+        h2 {
+            color: ${tan};
+            margin-top: -5px;
+        }
+
+        .desc {
+            text-transform: none;
+            letter-spacing: 0;
+            margin-bottom: 17px;
+            color: ${dark};
+            font-size: 1em;
+            line-height: 1.6em;
+            margin-right: 25px;
+            text-align: justify;
+        }
+
+        .buttons {
+            width: 100%;
+
+            .add {
+                width: 40%;
+                text-align: center;
+            }
+
+            .like {
+                width: 15%;
+                margin-left: 15px;
+                text-align: center;
+            }
         }
     }
-      
-    .add {
-        width: 67%;
-    }
-      
-    .like {
-        width: 22%;
-    }
-      
-    .sizes {
-        display: grid;
-        color: ${pink};
-        grid-template-columns: repeat(auto-fill, 30px);
-        width: 60%;
-        grid-gap: 4px;
-        margin-left: 20px;
-        margin-top: 5px;
 
-        &:hover {
-          cursor: pointer;
+    form {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+
+        select {
+            width: 37%;
+            border: ${rose};
+            border-radius: 2px;
+            padding: 2px;
         }
-    }
-      
-    .pick {
-        margin-top: 11px;
-        margin-bottom:0;
-        margin-left: 20px;
-    }
-      
-    .size {
-        padding: 9px;
-        border: 1px solid ${light};
-        font-size: 0.7em;
-        text-align: center;
 
-        &:hover{
-          background: ${rose};
-          color: ${white};
-          transition: all .4s ease-in-out;
+        .pick {
+            margin-top: 11px;
+            text-transform: uppercase;
+            color: ${rose};
         }
     }
       
@@ -125,30 +102,23 @@ export const Container = styled.div`
         background: ${rose};
         color: ${white};
     }
-      
-    footer {
-        position: absolute;
-        bottom: 0;
-        right: 0;
-        font-size: 0.8em;
-        text-transform: uppercase;
-        padding: 10px;
-
-        p {
-          letter-spacing: 3px;
-        }
-
-        a {
-          color: #ffffff;
-          text-decoration: none;
-
-          &:hover {
-            color: #7d7d7d;
-          }
-        }
-    }
 `;
 
 export const Button = styled.div`
-    
+    background: ${pink};
+    padding: 10px;
+    display: inline-block;
+    outline: 0;
+    border: 0;
+    margin: -1px;
+    border-radius: 2px;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    color: ${white};
+    cursor: pointer;
+
+    &:hover {
+      background: ${rose};
+      transition: all .4s ease-in-out;
+    }
 `;
